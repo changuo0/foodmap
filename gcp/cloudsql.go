7abf3a8ec6356413d748e208b5ab2934b1946280
Sam <sam.alws@gmail.com>
2021-03-03 19:57:07 +0000

    frontend backend connection

--- gcp/cloudsql.go
+++ gcp/cloudsql.go
@@ -1,53 +1,57 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START gae_cloudsql]
-
-// Sample cloudsql demonstrates connection to a Cloud SQL instance from App Engine standard.
 package main
 
-import (
-	"bytes"
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+// cd test-app
+// npm install
+// npm run build
+// cd ..
+// go run test.go
 
-	// MySQL library, comment out to use PostgreSQL.
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"os"
+	"log"
+	"bytes"
 	_ "github.com/go-sql-driver/mysql"
-	// PostgreSQL Library, uncomment to use.
-	// _ "github.com/lib/pq"
+	"database/sql"
 )
 
-var db *sql.DB
-
-func main() {
-	db = DB()
-
-	http.HandleFunc("/", indexHandler)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+type indexHtmlDefaultFs struct {
+	http.FileSystem
 }
+
+func (fs indexHtmlDefaultFs) Open(name string) (http.File, error) {
+	file, err := fs.FileSystem.Open(name)
+	if err != nil {
+		if name == "index.html" {
+			return nil, err
+		} else {
+			return fs.Open("index.html")
+		}
+	}
+	return file, err
+}
+
+// parse url of the form: "/ignore/a=b/x=sdaff/jskdfsdlkj=sdijs/"
+// tolerates redundant "/"s
+// returns nil if fails
+func parseUrl(ignore string, urlPath string) map[string]string {
+	urlMap := make(map[string]string)
+	for _, s := range strings.Split(urlPath,"/") {
+		if s == "" || s == ignore {
+			continue
+		}
+		spl := strings.Split(s, "=")
+		if len(spl) != 2 {
+			return nil
+		}
+		urlMap[spl[0]] = spl[1]
+	}
+	return urlMap
+}
+
+var db *sql.DB
 
 // DB gets a connection to the database.
 // This can panic for malformed database connection strings, invalid credentials, or non-existance database instance.
@@ -59,40 +63,46 @@ func DB() *sql.DB {
 		password       = os.Getenv("CLOUDSQL_PASSWORD")      // NOTE: password may be empty
 		socket         = os.Getenv("CLOUDSQL_SOCKET_PREFIX")
 	)
-
 	// /cloudsql is used on App Engine.
 	if socket == "" {
 		socket = "/cloudsql"
 	}
-
 	// MySQL Connection, comment out to use PostgreSQL.
 	// connection string format: USER:PASSWORD@unix(/cloudsql/PROJECT_ID:REGION_ID:INSTANCE_ID)/[DB_NAME]
 	dbURI := fmt.Sprintf("%s:%s@unix(%s/%s)/%s", user, password, socket, connectionName, dbName)
 	conn, err := sql.Open("mysql", dbURI)
-
-	// PostgreSQL Connection, uncomment to use.
-	// connection string format: user=USER password=PASSWORD host=/cloudsql/PROJECT_ID:REGION_ID:INSTANCE_ID/[ dbname=DB_NAME]
-	// dbURI := fmt.Sprintf("user=%s password=%s host=/cloudsql/%s dbname=%s", user, password, connectionName, dbName)
-	// conn, err := sql.Open("postgres", dbURI)
-
 	if err != nil {
 		panic(fmt.Sprintf("DB: %v", err))
 	}
-
 	return conn
 }
 
-// indexHandler responds to requests with our list of available databases.
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Panicf("%s environment variable not set.", k)
+	}
+	return v
+}
+
+// ASSUMPTION: database fields don't have "/"s or "="s in them
+// ANOTHER ASSUMPTION: sending db query over http doesnt compromise privacy
+func dbHandler(w http.ResponseWriter, r *http.Request) {
+	urlMap := parseUrl("db", r.URL.Path)
+	if urlMap == nil {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain")
-
-	db.Exec("USE foodmap;")
-	rows, err := db.Query("SELECT * FROM organizations;")
+	query := "SELECT * FROM organizations"
+	if len(urlMap) > 0 {
+		query += " WHERE "
+		for k,v := range urlMap {
+			query += k + " = " + v + " AND "
+		}
+		query = query[:len(query) - len(" AND ")]
+	}
+	query += ";"
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Printf("Could not query db: %v", err)
 		http.Error(w, "error at location 1: " + err.Error(), 500)
@@ -100,7 +110,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 
-	buf := bytes.NewBufferString("data:\n")
+	w.Header().Set("Content-Type", "text/plain")
+	buf := bytes.NewBufferString("")
 	for rows.Next() {
 		var name string
 		var contact string
@@ -114,17 +125,29 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "error at location 2: " + err.Error(), 500)
 			return
 		}
-		fmt.Fprintf(buf, "- %s, %s, %s, %s, %d, %s, %d\n", name, contact, notes, location, zip, foodtype, id)
+		fmt.Fprintf(buf, "%s, %s, %s, %s, %d, %s, %d\n", name, contact, notes, location, zip, foodtype, id)
 	}
 	w.Write(buf.Bytes())
 }
 
-func mustGetenv(k string) string {
-	v := os.Getenv(k)
-	if v == "" {
-		log.Panicf("%s environment variable not set.", k)
+// dont know exactly how this is gonna work
+func calHandler(w http.ResponseWriter, r *http.Request) {
+	urlMap := parseUrl("cal", r.URL.Path)
+	if urlMap == nil {
+		return
 	}
-	return v
+	// do some fancy calendar stuff
+	fmt.Println("eventually will look at calendar for following key value pairs:", urlMap)
+	fmt.Fprintf(w,"[]")
 }
 
-// [END gae_cloudsql]
+func main() {
+	db = DB()
+	db.Exec("USE foodmap;")
+
+	http.HandleFunc("/db/", dbHandler)
+	http.HandleFunc("/cal/", calHandler)
+	fs := indexHtmlDefaultFs{http.Dir("foodmap-frontend/build")}
+	http.Handle("/", http.FileServer(fs))
+	http.ListenAndServe(":8080", nil)
+}
